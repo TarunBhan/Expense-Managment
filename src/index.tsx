77155ef737e10bf947bbfbda509f202f11429656
TarunBhan <tarunbhanthakur@gmail.com>
2024-07-02 20:04:30 +0530

    push

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,12 @@ import UserProvider from "./context/UserProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
-  <React.StrictMode>
-    <UserProvider>
-      <App />
-    </UserProvider>
-  </React.StrictMode>
+  <div style={{ width: "100vw", height: "100vh", background: "red" }}></div>
+  // <React.StrictMode>
+  //   <UserProvider>
+  //     <App />
+  //   </UserProvider>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
